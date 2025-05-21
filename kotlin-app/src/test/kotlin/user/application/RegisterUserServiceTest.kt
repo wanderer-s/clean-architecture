0a3df30a16e83d6c8a96f6564721442457b8066f
wanderer-s <ows0871@gmail.com>
2025-05-22 02:47:11 +0900

    test code 추가

---
+++ kotlin-app/src/test/kotlin/user/application/RegisterUserServiceTest.kt
@@ -0,0 +1,36 @@
+package user.application
+
+import User
+import io.kotest.assertions.throwables.shouldThrow
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import user.adapter.`in`.web.RegisterUserRequest
+import user.adapter.out.persistence.UserPersistenceAdapter
+
+class RegisterUserServiceTest: DescribeSpec({
+    val userPersistenceAdapter = UserPersistenceAdapter()
+    val registerUserService = RegisterUserService(userPersistenceAdapter, userPersistenceAdapter)
+
+    val user = User("test@test.io", "test", "password")
+    userPersistenceAdapter.save(user)
+
+    describe("isRegisteredEmail - email로 가입된 내역 확인") {
+        it("입력된 email로 가입된 사용자가 없으면 false") {
+            val result = registerUserService.isRegisteredEmail("none@test.io")
+            result shouldBe false
+        }
+        it("입력된 email로 가입된 사용자가 있으면 true") {
+            val result = registerUserService.isRegisteredEmail("test@test.io")
+            result shouldBe true
+        }
+    }
+    describe("register - 회원 가입") {
+        it("이미 가입된 email 입력 시 예외 처리") {
+            shouldThrow<IllegalArgumentException> {
+                val userRequest = RegisterUserRequest("test@test.io", "test", "password")
+                registerUserService.register(userRequest)
+            }
+        }
+    }
+})
