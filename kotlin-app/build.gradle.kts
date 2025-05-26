61a322b330815411c14283bce324321a2c771f0a
wanderer-s <ows0871@gmail.com>
2025-05-27 01:59:07 +0900

    spring 의존성 처리

--- kotlin-app/build.gradle.kts
+++ kotlin-app/build.gradle.kts
@@ -1,9 +1,16 @@
 plugins {
-    kotlin("jvm") version "1.9.23"
+    id("org.springframework.boot") version "3.4.6"
+    id("io.spring.dependency-management") version "1.1.5"
+
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+
 
 repositories {
     mavenCentral()
@@ -17,11 +24,16 @@ dependencies {
     implementation("org.hibernate.validator:hibernate-validator:8.0.1.Final")
     implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.20")
 
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(17)
 }
